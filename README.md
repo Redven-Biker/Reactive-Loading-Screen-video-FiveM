9fe4e9125cdbb03b8d3be9ca0ae1c73e5a516865
Redven-Biker <marween35620@gmail.com>
2020-10-11 18:31:09 +0200

    1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+Discord : https://discord.gg/QW4TDJP
+
+![fivem](https://i.imgur.com/hITZLd6.jpg)
+
+Vidéo : https://youtu.be/gN16iW6-D3M
---
+++ __resource.lua
@@ -0,0 +1,13 @@
+files {
+    'index.html',
+    'css/style.css',
+    'js/*.js',
+    'img/background.jpg',
+    'music/loading.ogg'
+}
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
